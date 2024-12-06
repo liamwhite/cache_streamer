81abce116a7f8f0ac55967a1ab43a4f653dca11d
Liam <byteslice@airmail.cc>
2024-12-06 14:24:02 -0500

    hole tracker test

--- libs/sparse_map/src/hole_tracker.rs
+++ libs/sparse_map/src/hole_tracker.rs
@@ -17,3 +17,37 @@ impl From<HoleTracker> for Option<Range<usize>> {
         value.0.map(|(start, end)| start..end)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_default() {
+        let tracker = HoleTracker::default();
+        let result: Option<Range<usize>> = tracker.into();
+
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_single_range() {
+        let mut tracker = HoleTracker::default();
+        tracker.update(0, 1);
+
+        let result: Option<Range<usize>> = tracker.into();
+
+        assert_eq!(result, Some(0..1));
+    }
+
+    #[test]
+    fn test_multiple_ranges() {
+        let mut tracker = HoleTracker::default();
+        tracker.update(0, 1);
+        tracker.update(2, 3);
+
+        let result: Option<Range<usize>> = tracker.into();
+
+        assert_eq!(result, Some(0..3));
+    }
+}
