e98f3404ed32ba5741e2c30b66b4a003e3831a7a
Liam <byteslice@airmail.cc>
2024-12-14 15:49:45 -0500

    implement server

--- libs/cache_streamer_http/src/lib.rs
+++ libs/cache_streamer_http/src/lib.rs
@@ -2,6 +2,7 @@ pub use http_request_backend::HTTPRequestBackend;
 pub use http_requester::HTTPRequester;
 pub use http_response::HTTPResponse;
 pub use http_service::HTTPService;
+pub use reqwest::Url;
 
 mod header_util;
 mod http_request_backend;
